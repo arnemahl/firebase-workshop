9d8467fa97dbd5dc922cf1d7635113158eb61652
Arne Mæhlum <arne.mahlum@gmail.com>
2016-10-04 06:59:28 +0200

    CODE: Add logout TODO

--- src/pages/main/MainPage.jsx
+++ src/pages/main/MainPage.jsx
@@ -1,11 +1,38 @@
 import React from 'react';
 
+import Button from 'components/button/Button';
+
+// import { FIREBASE_APP } from 'MyFirebase'; // TODO: Authentication: Uncomment this line
+
 import './MainPage.scss';
 
-const MainPage = () => console.log('asdf') || (
-    <div className="page-main">
-        <h1>You are logged in!</h1>
-    </div>
-);
+class MainPage extends React.Component {
+
+    state = {
+        logoutErrorMessage: ''
+    }
+
+    onLogOut = () => {
+        // TODO: Authentication, log out
+        // Write code to log out of your Firebse app here
+        this.setState({
+            logoutErrorMessage: 'Logout is not implemented'
+        });
+    }
+
+    render() {
+        return (
+            <div className="page-main">
+                <h1>You are logged in!</h1>
+
+                <Button className="button-log-out" onClick={this.onLogOut}>
+                    Log out
+                </Button>
+
+                <div className="error-message">{this.state.logoutErrorMessage}</div>
+            </div>
+        );
+    }
+}
 
 export default MainPage;
